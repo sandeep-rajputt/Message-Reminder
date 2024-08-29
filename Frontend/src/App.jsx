171ada02e01f24f9830cbce92f6ac76bf8a0069d
sandeep-rajputt <sndp.rajpuut@gmail.com>
2024-08-29 14:22:31 +0530

    Add React Router DOM for routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ Frontend/src/assets/SVG Components/LogoSvg.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import logo from "../Svg/logo.svg";
+
+const LogoSvg = () => {
+  return <img src={logo} alt="Logo" />;
+};
+
+export default LogoSvg;
