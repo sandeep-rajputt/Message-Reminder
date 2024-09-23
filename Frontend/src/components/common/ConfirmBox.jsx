f98a4c0be904f1fc13170415fb499f16efc141b5
sandeep-rajputt <sndp.rajpuut@gmail.com>
2024-09-23 22:59:24 +0530

    Implement loader on Confirm button click

--- Frontend/src/components/common/ConfirmBox.jsx
+++ Frontend/src/components/common/ConfirmBox.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import Loader from "./Loader"; // Assuming Loader is your loading component
 
 const ConfirmBox = ({ handleConfirm, handleCancel, heading, text }) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleConfirmClick = async () => {
+    setLoading(true);
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    await handleConfirm();
+  };
+
   return (
     <div className="flex items-center justify-center h-screen w-screen bg-dark/70">
       <div className="bg-white p-4 border-dark-grey/40 rounded-md border w-96 max-w-[90%]">
@@ -16,10 +27,11 @@ const ConfirmBox = ({ handleConfirm, handleCancel, heading, text }) => {
             Cancel
           </button>
           <button
-            onClick={handleConfirm}
-            className="bg-primary border-dark bg-dark text-white  rounded px-2 py-1"
+            onClick={handleConfirmClick}
+            className="bg-primary border-dark bg-dark text-white rounded px-2 py-1"
+            disabled={loading} // Disable the button while loading
           >
-            Confirm
+            {loading ? <Loader /> : "Confirm"}
           </button>
         </div>
       </div>
